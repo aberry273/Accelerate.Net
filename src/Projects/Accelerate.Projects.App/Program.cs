902c6b8c1d92bee29bc490229379771f2dcceca2
ABR <aberry.consultancy@gmail.com>
2024-04-29 07:06:57 +1200

    #DEV - Debug app service

--- src/Projects/Accelerate.Projects.App/Program.cs
+++ src/Projects/Accelerate.Projects.App/Program.cs
@@ -106,7 +106,8 @@ if (app.Environment.IsDevelopment())
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
+    app.UseDeveloperExceptionPage();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
